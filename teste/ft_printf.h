8de5e2513aee6f809a5b1d15b0e11b2333732243
Vitor Sergio <vitorsergio111@gmail.com>
2022-06-08 07:08:42 -0300

    new direct

---
+++ teste/ft_printf.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 21:26:12 by vsergio           #+#    #+#             */
+/*   Updated: 2022/06/07 23:54:48 by vsergio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+size_t	ft_strlen(const char *str);
+
+int ft_showhex(unsigned long address, char *base);
+
+void	*ft_memset(void *b, int c, size_t len);
+
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+int		ft_atoi(const char *str);
+
+char	*ft_strdup(char *s1);
+
+int		ft_toupper(int c);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+
+int		ft_memcmp(const void *str1, const void *str2, size_t n);
+
+char	*ft_strchr(const char *s, int c);
+
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+
+char	*ft_strrchr(const char *s, int c);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+int		ft_tolower(int c);
+
+void	*ft_calloc(size_t count, size_t size);
+
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+char	*ft_strtrim(char const *s1, char const *set);
+
+int	ft_putchar(char c);
+
+int	ft_putstr(char *s);
+
+void	ft_putendl_fd(char *s, int fd);
+
+char	*ft_itoa(int n);
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+char	**ft_split(char const *s, char c);
+
+int	ft_putnbr(int n);
+
+#endif
