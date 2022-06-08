c3ef3ba61f007453fabcec8302bf6a53a16d3083
Vitor Marcondes <vitorsergio111@gmail.com>
2022-06-08 10:21:15 -0300

    changed files

--- ft_printf.c
+++ ft_printf.c
@@ -6,49 +6,29 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 23:21:09 by vsergio           #+#    #+#             */
-/*   Updated: 2022/06/07 00:31:33 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/06/07 11:02:09 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
+#include "ft_printf.h"
 #include <stdio.h>
-
-int ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	if (!str)
-	{
-		write (1, "(null)", 6);
-		return (6);
-	}
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-	return (i);
-}
-
-int ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
 
 int check_options(va_list list_arg, const char check_option)
 {
 	int printf_add;
 
 	printf_add = 0;
-	if (check_option == 's')
-		return (ft_putstr(va_arg(list_arg, char *)));
 	if (check_option == '%')
 		return (ft_putchar('%'));
 	else if(check_option == 'c')
 		return (ft_putchar(va_arg(list_arg, int)));
+	else if (check_option == 's')
+		return (ft_putstr(va_arg(list_arg, char *)));
+	// por que devemos converter o enderecamento para hexadecimal?
+	//pois o enderecamento eh binario (i.g) 0101001001010101, entao convertemos para hexadecimal
+	//para ser mais entendivel por um humano
+	// else if (check_option == 'p')
+		// return (ft_hex_adress(va_arg(list_arg, unsigned long)));
 	else
 		return (0);
 }
@@ -79,10 +59,13 @@ int ft_printf(const char *options, ...)
 
 int main(void)
 {
+	char rato = 'c';
 	char *nome = "Viitor";
+	int testeadress = ft_printf("%p", &rato);
 	int testeretorno = ft_printf("printf de teste: %s\n", nome);
-	ft_printf("eae %s", nome);
+	ft_printf("eae %s %p", nome);
 	printf("\n");
 	printf("eae %s\n", nome);
+	printf("retorno adress: %i\n", testeadress);
 	printf("retorno do teste: %i", testeretorno);
 }
